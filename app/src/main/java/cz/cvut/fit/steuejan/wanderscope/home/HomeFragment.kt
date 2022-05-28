14a2a9580b3bae147efcf17192257e8bc4d0d7fa
Jan Steuer <jan.steuer@centrum.cz>
2022-05-28 10:49:49 +0200

    Get recommended trip in home screen

--- app/src/main/java/cz/cvut/fit/steuejan/wanderscope/home/HomeFragment.kt
+++ app/src/main/java/cz/cvut/fit/steuejan/wanderscope/home/HomeFragment.kt
@@ -2,9 +2,11 @@ package cz.cvut.fit.steuejan.wanderscope.home
 
 import android.os.Bundle
 import android.view.View
+import com.facebook.shimmer.ShimmerFrameLayout
 import cz.cvut.fit.steuejan.wanderscope.MainActivityVM
 import cz.cvut.fit.steuejan.wanderscope.R
 import cz.cvut.fit.steuejan.wanderscope.app.arch.mwwm.MvvmFragment
+import cz.cvut.fit.steuejan.wanderscope.app.bussiness.loading.WithLoading
 import cz.cvut.fit.steuejan.wanderscope.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
@@ -12,13 +14,27 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 class HomeFragment : MvvmFragment<FragmentHomeBinding, HomeFragmentVM>(
     R.layout.fragment_home,
     HomeFragmentVM::class
-) {
+), WithLoading {
+
+    override val hasToolbar = false
+
+    override val content: View get() = binding.homeContent
+    override val shimmer: ShimmerFrameLayout get() = binding.homeShimmer
 
     private val mainVM by sharedViewModel<MainActivityVM>()
 
+    private var init = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        retrieveRecommendedTrip()
         handleLogin()
+        handleLoading()
+    }
+
+    private fun retrieveRecommendedTrip() {
+        viewModel.getRecommendedTrip(getString(R.string.no_upcoming_trips_title), init)
+        init = false
     }
 
     private fun handleLogin() {
@@ -27,6 +43,15 @@ class HomeFragment : MvvmFragment<FragmentHomeBinding, HomeFragmentVM>(
                 login()
             } else {
                 mainVM.hideSplashScreen()
+            }
+        }
+    }
+
+    private fun handleLoading() {
+        showLoading()
+        viewModel.loading.safeObserve { loading ->
+            if (!loading) {
+                hideLoading()
             }
         }
     }
--- app/src/main/java/cz/cvut/fit/steuejan/wanderscope/home/HomeFragmentVM.kt
+++ app/src/main/java/cz/cvut/fit/steuejan/wanderscope/home/HomeFragmentVM.kt
@@ -1,14 +1,71 @@
 package cz.cvut.fit.steuejan.wanderscope.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import cz.cvut.fit.steuejan.wanderscope.app.arch.BaseViewModel
+import cz.cvut.fit.steuejan.wanderscope.app.bussiness.loading.LoadingMediator
+import cz.cvut.fit.steuejan.wanderscope.app.common.Constants
+import cz.cvut.fit.steuejan.wanderscope.app.common.Result
+import cz.cvut.fit.steuejan.wanderscope.app.common.data.Duration
+import cz.cvut.fit.steuejan.wanderscope.app.common.recycler_item.DurationString
+import cz.cvut.fit.steuejan.wanderscope.app.extension.delayAndReturn
+import cz.cvut.fit.steuejan.wanderscope.app.extension.launchIO
+import cz.cvut.fit.steuejan.wanderscope.app.extension.safeCollect
+import cz.cvut.fit.steuejan.wanderscope.app.extension.toDurationString
+import cz.cvut.fit.steuejan.wanderscope.app.retrofit.response.Error
 import cz.cvut.fit.steuejan.wanderscope.auth.repository.AuthRepository
+import cz.cvut.fit.steuejan.wanderscope.trips.api.response.TripsResponse
+import cz.cvut.fit.steuejan.wanderscope.trips.model.TripsScope
+import cz.cvut.fit.steuejan.wanderscope.trips.repository.TripsRepository
 
 class HomeFragmentVM(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val tripsRepository: TripsRepository
 ) : BaseViewModel() {
 
     val shouldLogin = liveData {
         emit(!authRepository.isUserLoggedIn())
+    }
+
+    val duration = MutableLiveData<DurationString>()
+    val title = MutableLiveData<String>()
+    val seeMoreVisibility = MutableLiveData<Boolean>()
+
+    private val recommendedTripLoading = MutableLiveData<Boolean>()
+
+    val loading = LoadingMediator(
+        recommendedTripLoading
+    ).delayAndReturn(Constants.DELAY_LOADING)
+
+    fun getRecommendedTrip(emptyTitle: String, init: Boolean) {
+        viewModelScope.launchIO {
+            tripsRepository.getTrips(TripsScope.RECOMMENDED).safeCollect(this) {
+                when (it) {
+                    is Result.Cache -> TODO()
+                    is Result.Failure -> failure(it.error, recommendedTripLoading)
+                    is Result.Loading -> recommendedTripLoading.value = true
+                    is Result.Success -> getRecommendedTripSuccess(it.data, emptyTitle)
+                }
+            }
+        }
+    }
+
+    private fun failure(error: Error, loading: MutableLiveData<Boolean>) {
+        loading.value = false
+        unexpectedError(error)
+    }
+
+    private suspend fun getRecommendedTripSuccess(data: TripsResponse, emptyTitle: String) {
+        if (data.trips.isEmpty()) {
+            seeMoreVisibility.value = false
+            duration.value = Duration().toDurationString()
+            title.value = emptyTitle
+        } else with(data.trips.first()) {
+            this@HomeFragmentVM.duration.value = duration.toDurationString()
+            title.value = name
+            seeMoreVisibility.value = true
+        }
+        recommendedTripLoading.value = false
     }
 }
